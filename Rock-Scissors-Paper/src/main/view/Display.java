7af4aff6d7262e24e11ac46975d886bf6845c67a
Hyunjin Cha <164485306+hyunjin4270@users.noreply.github.com>
2025-04-23 17:33:54 +0900

    FEAT: View 문서 수정 및 분할화 작업

--- Rock-Scissors-Paper/src/main/view/Display.java
+++
@@ -1,9 +0,0 @@
-package main.view;
-
-import main.domain.player.User;
-
-public interface Display {
-    void printTurn(User user);
-
-    void printWinner(User user);
-}
---
+++ Rock-Scissors-Paper/src/main/view/PlayerList.java
@@ -0,0 +1,16 @@
+package main.view;
+
+import main.controller.InputHandler;
+import main.controller.OutputHandler;
+
+public class PlayerList {
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
+
+    public PlayerList(InputHandler inputHandler, OutputHandler outputHandler) {
+        this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
+    }
+
+    public void PlayerList()
+}
